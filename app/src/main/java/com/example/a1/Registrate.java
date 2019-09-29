872c6d77d0e48d39acc305037496b3226b68dd15
BezlepkinAndrey <andbezlepkin@rambler.ru>
2019-09-29 11:53:43 +0300

    init

--- app/src/main/java/com/example/a1/Registrate.java
+++ app/src/main/java/com/example/a1/Registrate.java
@@ -1,20 +1,16 @@
 package com.example.a1;
-
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Registrate extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
     public void onMyButtonClick(View view)
     {
-        Intent intent = new Intent(MainActivity.this, show_list.class);
-        startActivity(intent);
+
     }
 }
